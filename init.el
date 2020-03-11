9577eac993d46a591a97106e6479f6ecb6309222
Pieter Minnaar <pieter.minnaar@gmail.com>
2020-03-11 10:01:05 +0100

    highlight brackets

--- init.el
+++ init.el
@@ -12,15 +12,6 @@
 
 (iswitchb-mode 1)
 
-(iswitchb-mode 1)
- ;; (defun iswitchb-local-keys ()
- ;;  (mapc (lambda (K)
- ;; (let* ((key (car K)) (fun (cdr K)))
- ;;             (define-key iswitchb-mode-map (edmacro-parse-keys key) fun)))
- ;;           '(("<right>" . iswitchb-next-match)
- ;;             ("<left>"  . iswitchb-prev-match)
- ;;             ("<up>"    . ignore             )
- ;;             ("<down>"  . ignore             ))))
 
 (defun iswitchb-local-keys ()
  (mapc (lambda (K)
@@ -130,3 +121,5 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+(show-paren-mode 1)
